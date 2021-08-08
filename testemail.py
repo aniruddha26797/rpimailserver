c4c45a4f954506c65cbb273a1e28b9a3a99c7ad7
aniruddha26797 <47391398+aniruddha26797@users.noreply.github.com>
2021-08-07 23:30:41 -0700

    Create testemail.py

---
+++ testemail.py
@@ -0,0 +1,29 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+email_user = "account@gmail.com"
+email_password = "password"
+email_send = "adith004@umn.edu"
+
+subject = "Test email from pi"
+
+msg = MIMEMultipart()
+msg["From"] = email_user
+msg["To"] = email_send
+msg["Subject"] = subject
+
+body = "Hi there, sending this email from Python!"
+msg.attach(MIMEText(body,"plain"))
+
+text = msg.as_string()
+server = smtplib.SMTP("smtp.umn.edu",465)
+server.starttls()
+server.login(email_user,email_password)
+
+
+server.sendmail(email_user,email_send,text)
+server.quit()
+We want information… information… information.........
